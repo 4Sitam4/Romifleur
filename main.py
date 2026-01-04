60ad72e1371b0e1633d0dd8ca8c288bfda346a3e
Mathis B. <109106334+4Sitam4@users.noreply.github.com>
2026-01-04 20:38:13 +0100

    Cleaned my mistakes
    Bumped the VERSION variable from v1.0.1 to v1.0.4 in main.py to reflect the latest release.

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ class App(ctk.CTk):
     def __init__(self):
         super().__init__()
         
-        self.VERSION = "v1.0.1"
+        self.VERSION = "v1.0.4"
 
         # Window
         self.title(f"Romifleur {self.VERSION}")
